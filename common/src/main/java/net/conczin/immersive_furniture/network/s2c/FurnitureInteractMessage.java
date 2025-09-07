06a9ffffd0b19b5a379a01ccb17ac1b8f6ee34b9
Luke100000 <jonas.peche@aon.at>
2025-09-07 18:54:18 +0200

    ported

--- common/src/main/java/net/conczin/immersive_furniture/network/s2c/FurnitureInteractMessage.java
+++ common/src/main/java/net/conczin/immersive_furniture/network/s2c/FurnitureInteractMessage.java
@@ -4,14 +4,16 @@ import net.conczin.immersive_furniture.Common;
 import net.conczin.immersive_furniture.network.ImmersivePayload;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.world.entity.player.Player;
 
-public record FurnitureInteractMessage(BlockPos pos) implements ImmersivePayload {
+public record FurnitureInteractMessage(BlockPos pos, boolean active) implements ImmersivePayload {
     public static final CustomPacketPayload.Type<FurnitureInteractMessage> TYPE = new CustomPacketPayload.Type<>(Common.locate("furniture_interact_message"));
     public static final StreamCodec<FriendlyByteBuf, FurnitureInteractMessage> STREAM_CODEC = StreamCodec.composite(
             BlockPos.STREAM_CODEC, FurnitureInteractMessage::pos,
+            ByteBufCodecs.BOOL, FurnitureInteractMessage::active,
             FurnitureInteractMessage::new
     );
 
